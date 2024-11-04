87e5156637c651a7b3227928381c9e6c47f3109c
Bryan Head <bryan.head@gmail.com>
2024-11-04 06:29:49 -0800

    Delay: Switch to arbitrary size mixer

--- software/src/audio/AudioPassthrough.h
+++ software/src/audio/AudioPassthrough.h
@@ -2,21 +2,20 @@
 #include <Audio.h>
 
 template <uint8_t NumChannels>
-class AudioPassthrough : public AudioStream
-{
+class AudioPassthrough : public AudioStream {
 public:
   AudioPassthrough() : AudioStream(NumChannels, inputQueueArray) {}
   virtual void update(void) {
-    audio_block_t *block;
-    for(int i=0; i<NumChannels; i++) {
+    audio_block_t* block;
+    for (int i = 0; i < NumChannels; i++) {
       block = receiveReadOnly(i);
-      if(block) {
+      if (block) {
         transmit(block, i);
         release(block);
       }
     }
   }
-private:
-  audio_block_t *inputQueueArray[NumChannels];
-};
 
+private:
+  audio_block_t* inputQueueArray[NumChannels];
+};
